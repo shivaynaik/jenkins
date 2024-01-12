9ad56768ccda90dac5eb1da0bd2a2bd0c020b64a
GANESH0369 <110584740+GANESH0369@users.noreply.github.com>
2024-01-12 16:27:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, jenkins pipeline with git'
+    return 'Hello, jenkins pipeline with git hooks'
 
 if __name__ == '__main__':
     app.run(debug=True)
